7325c5656346baab3da00140c8d504f525f870f2
David Johnston <david@blacksheepcode.com>
2024-08-20 13:13:19 +1000

    Add tsconfig.build.json

---
+++ dist/library/MyComponent.d.ts
@@ -0,0 +1,1 @@
+export declare function MyComponent(): import("react/jsx-runtime").JSX.Element;
---
+++ dist/library/MyComponent.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MyComponent = MyComponent;
+var jsx_runtime_1 = require("react/jsx-runtime");
+function MyComponent() {
+    return (0, jsx_runtime_1.jsx)("div", { children: "This is the component" });
+}
